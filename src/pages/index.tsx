1dc1292b5013b409ba1d509f40cabd747c4953a4
Alex Lewis <alexvlewis@gmail.com>
2024-12-03 13:38:56 -0800

    Upgrade next/font

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import sb from 'satoshi-bitcoin';
-import { Inter } from '@next/font/google';
+import { Inter } from "next/font/google";
 import styles from '@/styles/Home.module.css';
 import { useCallback, useEffect, useRef, useState } from 'react';
 
